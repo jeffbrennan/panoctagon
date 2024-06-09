c07cf671b95f1cd3dd4eb51d2b2a2983de4429ee
Jeff Brennan <jeffbrennan10@gmail.com>
2024-06-09 13:34:02 -0400

    update helpers - relative con path and table row extractor

--- panoctagon/common.py
+++ panoctagon/common.py
@@ -1,5 +1,12 @@
 import sqlite3
 from dataclasses import dataclass, astuple, fields
+from pathlib import Path
+import bs4
+
+@dataclass(frozen=True)
+class Promotion:
+    promotion_uid: str
+    name: str
 
 
 def write_tuples_to_db(con: sqlite3.Connection, tbl_name: str, data: list[dataclass]):
@@ -13,3 +20,25 @@ def write_tuples_to_db(con: sqlite3.Connection, tbl_name: str, data: list[datacl
     query = f"INSERT INTO {tbl_name} ({headers}) VALUES ({placeholders})"
     cur.executemany(query, tuples)
     con.commit()
+
+
+def get_con() -> tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_path = Path(__file__).parent.parent / "data" / "panoctagon.db"
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    return con, cur
+
+
+def get_table_rows(soup: bs4.BeautifulSoup) -> bs4.ResultSet:
+    table = soup.find("table")
+    if table is None:
+        raise ValueError("No table found")
+
+    table_body = table.find("tbody")
+    if not isinstance(table_body, bs4.Tag):
+        raise TypeError(f"expected bs4.Tag, got {type(table_body)}")
+
+    rows = table_body.find_all("tr")
+    if rows is None:
+        raise ValueError()
+    return rows
