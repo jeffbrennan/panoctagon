6fa4ec52d1cfdc43a46fdc6ad368e45dabbff1b0
Jeff Brennan <jeffbrennan10@gmail.com>
2024-06-18 23:28:49 -0400

    add scraping config for generic downloading of html files to a local directory

--- panoctagon/ufc/scrape_fights.py
+++ panoctagon/ufc/scrape_fights.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import bs4
 import requests
 
-from panoctagon.common import get_table_rows
+from panoctagon.common import get_table_rows, ScrapingConfig, dump_html
 
 
 def read_event_uids():
@@ -15,17 +15,6 @@ def read_event_uids():
     uids = [i[0] for i in cur.fetchall()]
     return uids
 
-
-def dump_fight_html(fight_uid: str) -> None:
-    print(f"saving fight: {fight_uid}")
-    url = f"http://www.ufcstats.com/fight-details/{fight_uid}"
-    output_path = Path(__file__).parents[2] / f"data/raw/ufc/fights/{fight_uid}.html"
-
-    response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.text, "html.parser")
-
-    with output_path.open("w") as f:
-        f.write(str(soup))
 
 
 def get_list_of_fights(soup: bs4.BeautifulSoup) -> list[str]:
@@ -47,21 +36,35 @@ def get_list_of_fights(soup: bs4.BeautifulSoup) -> list[str]:
     return fight_uids
 
 
-def get_fights_from_event(uid):
+def get_fights_from_event(uid: str, base_dir: Path) -> None:
     url = f"http://www.ufcstats.com/event-details/{uid}"
     response = requests.get(url).content
     soup = bs4.BeautifulSoup(response, "html.parser")
     fight_uids = get_list_of_fights(soup)
+
+    configs = [
+        ScrapingConfig(
+            uid=i,
+            description="fight",
+            base_url="http://www.ufcstats.com/fight-details",
+            base_dir=base_dir,
+            fname=i,
+        )
+        for i in fight_uids
+    ]
     with ThreadPoolExecutor(max_workers=4) as executor:
-        executor.map(dump_fight_html, fight_uids)
+        executor.map(dump_html, configs)
 
 
 def main() -> None:
     event_uids = read_event_uids()
     n_events = len(event_uids)
+    base_dir = Path(__file__).parents[2] / "data" / "raw" / "ufc" / "fights"
+    base_dir.mkdir(exist_ok=True)
+
     for i, uid in enumerate(event_uids, 1):
         print(f"[{i:03d}/{n_events:03d}] processing event : {uid}")
-        get_fights_from_event(uid)
+        get_fights_from_event(uid, base_dir)
 
 
 if __name__ == "__main__":
