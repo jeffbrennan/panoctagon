121e6af496943a4d010178f960c73202fd799e28
jeffbrennan <jeffbrennan10@gmail.com>
2024-06-21 21:53:28 -0400

    add wip first time run script

---
+++ panoctagon/utils/setup_db.py
@@ -0,0 +1,11 @@
+from panoctagon.promotions import setup_promotions
+from panoctagon.divisions import setup_divisions
+
+
+def main() -> None:
+    setup_promotions()
+    setup_divisions()
+
+
+if __name__ == '__main__':
+    main()
