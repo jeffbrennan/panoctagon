0696003000dc959902f5ad6faad9a90bd2b1d317
Jeff Brennan <jeffbrennan10@gmail.com>
2024-07-27 09:44:54 -0400

    add initial stats visualization by fighter

--- panoctagon/app.py
+++ panoctagon/app.py
@@ -2,22 +2,71 @@ from dash import Dash, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_mantine_components as dmc
+from panoctagon.common import get_engine
+from typing import Any
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"
+df = pd.read_sql_query(
+    """
+    select
+        fs.fight_uid,
+        fs.round_num,
+        fs.fighter_uid,
+        fs.total_strikes_landed,
+        fs.total_strikes_attempted,
+        fs.takedowns_landed,
+        fs.takedowns_attempted,
+        coalesce(f1result.fighter1_result, f2result.fighter2_result) as fighter_result,
+        f.first_name,
+        f.last_name,
+        f.first_name || ' ' || f.last_name as fighter_name,
+        f.height_inches,
+        f.reach_inches,
+        e.event_date,
+        e.title
+    from ufc_fight_stats fs
+    inner join ufc_fighters f
+        on fs.fighter_uid = f.fighter_uid
+    inner join ufc_fights fights
+        on fs.fight_uid = fights.fight_uid
+    inner join ufc_events e
+        on fights.event_uid = e.event_uid
+    left join ufc_fights f1result
+        on fs.fighter_uid = f1result.fighter1_uid
+        and fs.fight_uid = f1result.fight_uid
+    left join ufc_fights f2result
+        on fs.fighter_uid = f2result.fighter2_uid
+        and fs.fight_uid = f2result.fight_uid
+    """,
+    get_engine(),
 )
-
+tbl_cols = [
+    "title",
+    "fighter_result",
+    "round_num",
+    "total_strikes_landed",
+    "total_strikes_attempted",
+    "takedowns_landed",
+    "takedowns_attempted",
+]
 app = Dash()
 
 app.layout = dmc.Container(
     [
-        dmc.Title(
-            "My First App with Data, Graph, and Controls", color="blue", size="h3"
+        dmc.Title("Panoctagon", color="blue", size="h3"),
+        dmc.TextInput(
+            w="200",
+            placeholder="Enter Fighter Name",
+            label="Fighter Name",
+            id="fighter_name",
+            value=df.sample(1)["fighter_name"].item(),
         ),
         dmc.RadioGroup(
-            [dmc.Radio(i, value=i) for i in ["pop", "lifeExp", "gdpPercap"]],
+            [
+                dmc.Radio(i, value=i)
+                for i in ["total_strikes_landed", "takedowns_landed"]
+            ],
             id="my-dmc-radio-item",
-            value="lifeExp",
+            value="total_strikes_landed",
             size="sm",
         ),
         dmc.Grid(
@@ -25,14 +74,20 @@ app.layout = dmc.Container(
                 dmc.Col(
                     [
                         dash_table.DataTable(
-                            data=df.to_dict("records"),
-                            page_size=12,
-                            style_table={"overflowX": "auto"},
+                            id="table-placeholder",
+                            columns=[{"name": i, "id": i} for i in df[tbl_cols].columns],
+                            sort_action="native",
+                            filter_action="native",
+                            style_table={
+                                "height": "500px",
+                                "overflowX": "scroll",
+                                "overflowY": "scroll",
+                            },
                         )
                     ],
-                    span=6,
+                    span=4,
                 ),
-                dmc.Col([dcc.Graph(figure={}, id="graph-placeholder")], span=6),
+                dmc.Col([dcc.Graph(figure={}, id="graph-placeholder")], span=8),
             ]
         ),
     ],
@@ -41,11 +96,53 @@ app.layout = dmc.Container(
 
 
 @callback(
+    Output(component_id="table-placeholder", component_property="data"),
+    Input(component_id="fighter_name", component_property="value"),
+)
+def update_table(fighter_name: str) -> list[dict[Any, Any]]:
+    fighter_name = fighter_name.strip().title()
+    tbl_cols = [
+        "title",
+        "fighter_result",
+        "round_num",
+        "total_strikes_landed",
+        "total_strikes_attempted",
+        "takedowns_landed",
+        "takedowns_attempted",
+    ]
+
+    df_filtered = df[df["fighter_name"].str.strip().str.title() == fighter_name][
+        tbl_cols
+    ]
+
+    if df_filtered.empty:
+        return [{}]
+
+    if not isinstance(df_filtered, pd.DataFrame):
+        raise TypeError()
+
+    return df_filtered.to_dict("records")
+
+
+@callback(
     Output(component_id="graph-placeholder", component_property="figure"),
     Input(component_id="my-dmc-radio-item", component_property="value"),
+    Input(component_id="fighter_name", component_property="value"),
 )
-def update_graph(col_chosen):
-    fig = px.histogram(df, x="continent", y=col_chosen, histfunc="avg")
+def update_graph(metric: str, fighter_name: str):
+    fighter_name = fighter_name.strip().title()
+    df_filtered = df[df["fighter_name"].str.strip().str.title() == fighter_name]
+
+    if df_filtered.empty:
+        fig = px.strip(title=f"No data for {fighter_name}")
+    else:
+        fig = px.strip(
+            data_frame=df_filtered,
+            x="event_date",
+            y=metric,
+            color="fighter_result",
+            title=f"{fighter_name} - {metric}",
+        )
     return fig
 
 
