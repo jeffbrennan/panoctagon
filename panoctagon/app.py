fa7f9e3a59d9afe68262055db8a1ecee4c1d8077
Jeff Brennan <jeffbrennan10@gmail.com>
2024-08-07 23:07:44 -0400

    update base query, standardize table columns

--- panoctagon/app.py
+++ panoctagon/app.py
@@ -2,52 +2,132 @@ from dash import Dash, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_mantine_components as dmc
-from panoctagon.common import get_engine
+from panoctagon.common import get_engine, get_table_rows
 from typing import Any
 
+
+def get_tbl_cols() -> list[str]:
+    tbl_cols = [
+        "title",
+        "fighter_result",
+        "opponent_name",
+        "round_num",
+        "total_strikes_landed",
+        "total_strikes_attempted",
+        "takedowns_landed",
+        "takedowns_attempted",
+    ]
+    return tbl_cols
+
+
+# TODO: precompute this
 df = pd.read_sql_query(
     """
+    with
+        event_details      as (
+            select
+                title,
+                event_date,
+                fight_uid
+            from ufc_events a
+            inner join ufc_fights b
+                on a.event_uid = b.event_uid
+        ),
+        fighter_details    as (
+            select
+                fighter_uid,
+                first_name,
+                last_name,
+                height_inches,
+                reach_inches
+            from ufc_fighters
+        ),
+        fight_results_long as (
+            select
+                fight_uid,
+                fighter1_uid    as fighter_uid,
+                fighter1_result as fighter_result
+            from ufc_fights
+            union
+            select
+                fight_uid,
+                fighter2_uid    as fighter_uid,
+                fighter2_result as fighter_result
+            from ufc_fights
+        ),
+        fs                 as (
+            select
+                a.fight_uid,
+                a.round_num,
+                a.fighter_uid,
+                c.fighter_result,
+                b.first_name || ' ' || b.last_name as fighter_name,
+                b.reach_inches,
+                b.height_inches,
+                total_strikes_landed,
+                total_strikes_attempted,
+                takedowns_landed,
+                takedowns_attempted
+            from ufc_fight_stats a
+            inner join fighter_details b
+                on a.fighter_uid = b.fighter_uid
+            inner join fight_results_long c
+                on a.fight_uid = c.fight_uid
+                and a.fighter_uid = c.fighter_uid
+        ),
+        opp_fs             as (
+            select
+                a.fight_uid,
+                a.round_num,
+                a.fighter_uid                      as opponent_uid,
+                c.fighter_result                   as opponent_result,
+                b.first_name || ' ' || b.last_name as opponent_name,
+                b.reach_inches                     as opponent_reach_inches,
+                b.height_inches                    as opponent_height_inches,
+                total_strikes_landed               as opponent_strikes_landed,
+                total_strikes_attempted            as opponent_strikes_attempted,
+                takedowns_landed                   as opponent_takedowns_landed,
+                takedowns_attempted                as opponent_takedowns_attempted
+            from ufc_fight_stats a
+            inner join fighter_details b
+                on a.fighter_uid = b.fighter_uid
+            inner join fight_results_long c
+                on a.fight_uid = c.fight_uid
+                and a.fighter_uid = c.fighter_uid
+
+        )
     select
+        event_details.title,
+        event_details.event_date,
         fs.fight_uid,
         fs.round_num,
-        fs.fighter_uid,
-        fs.total_strikes_landed,
-        fs.total_strikes_attempted,
-        fs.takedowns_landed,
-        fs.takedowns_attempted,
-        coalesce(f1result.fighter1_result, f2result.fighter2_result) as fighter_result,
-        f.first_name,
-        f.last_name,
-        f.first_name || ' ' || f.last_name as fighter_name,
-        f.height_inches,
-        f.reach_inches,
-        e.event_date,
-        e.title
-    from ufc_fight_stats fs
-    inner join ufc_fighters f
-        on fs.fighter_uid = f.fighter_uid
-    inner join ufc_fights fights
-        on fs.fight_uid = fights.fight_uid
-    inner join ufc_events e
-        on fights.event_uid = e.event_uid
-    left join ufc_fights f1result
-        on fs.fighter_uid = f1result.fighter1_uid
-        and fs.fight_uid = f1result.fight_uid
-    left join ufc_fights f2result
-        on fs.fighter_uid = f2result.fighter2_uid
-        and fs.fight_uid = f2result.fight_uid
+        fighter_name,
+        fighter_result,
+        height_inches,
+        reach_inches,
+        total_strikes_landed,
+        total_strikes_attempted,
+        takedowns_landed,
+        takedowns_attempted,
+        opponent_name,
+        opponent_result,
+        opponent_reach_inches,
+        opponent_height_inches,
+        opponent_strikes_attempted,
+        opponent_strikes_landed,
+        opponent_takedowns_attempted,
+        opponent_takedowns_landed
+    from fs
+    inner join event_details
+        on fs.fight_uid = event_details.fight_uid
+    inner join opp_fs
+        on fs.fight_uid = opp_fs.fight_uid
+        and fs.round_num = opp_fs.round_num
+        and fs.fighter_uid != opp_fs.opponent_uid;
     """,
     get_engine(),
 )
-tbl_cols = [
-    "title",
-    "fighter_result",
-    "round_num",
-    "total_strikes_landed",
-    "total_strikes_attempted",
-    "takedowns_landed",
-    "takedowns_attempted",
-]
+
 initial_fighter = df.sample(1)["fighter_name"].item()
 if not isinstance(initial_fighter, str):
     raise TypeError()
@@ -77,7 +157,7 @@ app.layout = dmc.Container(
             [
                 dash_table.DataTable(
                     id="table-placeholder",
-                    columns=[{"name": i, "id": i} for i in df[tbl_cols].columns],
+                    columns=[{"name": i, "id": i} for i in df[get_tbl_cols()].columns],
                     sort_action="native",
                     filter_action="native",
                     style_table={
@@ -99,21 +179,11 @@ app.layout = dmc.Container(
 )
 def update_table(fighter_name: str) -> list[dict[Any, Any]]:
     fighter_name = fighter_name.strip().title()
-    tbl_cols = [
-        "title",
-        "fighter_result",
-        "round_num",
-        "total_strikes_landed",
-        "total_strikes_attempted",
-        "takedowns_landed",
-        "takedowns_attempted",
-    ]
-
     df_filtered = (
         df.assign(fighter_name=lambda x: x.fighter_name.str.strip().str.title())  # type: ignore
         .query(f"fighter_name == '{fighter_name}'")
         .sort_values("event_date", ascending=False)
-    )[tbl_cols]
+    )[get_tbl_cols()]
 
     if df_filtered.empty:
         return [{}]
